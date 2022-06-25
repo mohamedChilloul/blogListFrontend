844e4f739a6b7b02cb5f917605be43e60006e9bb
mohamedChilloul <m.chilloul@esi-sba.dz>
2022-06-26 00:09:22 +0100

    5.1: bloglist frontend, step1

---
+++ src/services/login.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const basedUrl = '/api/login'
+
+const login = async (credentials)=>{
+    const request = await axios.post(basedUrl, credentials)
+    return request.data
+}
+
+export default {
+    login
+}
